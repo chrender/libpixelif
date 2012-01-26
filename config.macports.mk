eb471cd769d278d1374eea86fd137c0bb504dbef
chrender <chrender>
2012-01-26 13:03:18 +0000

    - Added config.*.mk files.

---
+++ config.macports.mk
@@ -0,0 +1,27 @@
+
+CC = gcc
+AR = ar
+override CFLAGS += -Wall -Wextra
+
+prefix = $(DESTDIR)/opt/local
+bindir = $(prefix)/bin
+datarootdir = $(prefix)/share
+mandir = $(datarootdir)/man
+localedir = $(datarootdir)/fizmo/locales
+
+
+# -----
+# General settings:
+ENABLE_OPTIMIZATION = 1
+#ENABLE_TRACING = 1
+#ENABLE_GDB_SYMBOLS = 1
+# -----
+
+
+
+# -----
+# Settings for libpixelif:
+LIBFREETYPE2_CFLAGS = $(shell freetype-config --cflags)
+LIBFREETYPE2_LIBS = $(shell freetype-config --libs)
+# -----
+
